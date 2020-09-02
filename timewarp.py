336db6138eec5dabe66babf5ad3e269ac650468f
Jun Rekimoto <rekimoto@acm.org>
2020-09-02 14:10:29 +0900

    timewarp.py

--- timewarp.py
+++ timewarp.py
@@ -5,6 +5,7 @@ import os
 
 fname = 'tennis.mp4'
 slow = 2
+# 遅くするフレームの範囲
 slow_range = [[45, 74], [110, 139], [173, 216], [254, 284]]
 
 
@@ -21,9 +22,7 @@ while cap.isOpened():
     a = cv2.imwrite('/tmp/video%04d.jpg' % (j,), frame)
     j = j + 1
         
-    #if (45 <= i and i <= 77) or (114 <= i and i <= 142) or (176 <= i and i <= 218) :
     if True in [x[0] <= i < x[1] for x in slow_range]:
-#    if (45 <= i and i <= 74) or (110 <= i and i <= 139) or (173 <= i and i <= 216) or (254 <= i and i <= 284):
         for k in range(0, slow):
             cv2.imwrite('/tmp/video%04d.jpg' % (j,), frame)
             j = j + 1
